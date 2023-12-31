f3ce6e33700d924cfa3b0007f579c597a784713d
hadigghazi <128805519+hadigghazi@users.noreply.github.com>
2023-12-31 11:55:35 +0200

    initial commit

---
+++ assets/js/clothes.js
@@ -0,0 +1,32 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SWIPER PRODUCTS ===============*/
+let swiperProducts = new Swiper('.products__container', {
+    cssMode: true,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
